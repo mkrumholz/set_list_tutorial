35c461147e92c67dbcc18d1818f3a885a8e9b3ff
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-12 16:20:48 -0600

    Create Artist class and update test

--- spec/features/songs/show_spec.rb
+++ spec/features/songs/show_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe 'songs show' do
   before(:each) do
     artist = Artist.create(name: 'Carly Rae Jepsen')
-    @song_1 = Song.create!(title:       "I Really Like You",
+    @song_1 = artist.songs.create!(title: "I Really Like You",
                          length:      208,
                          play_count:  243810867)
-    @song_2 = Song.create!(title:       "Call Me Maybe",
+    @song_2 = artist.songs.create!(title: "Call Me Maybe",
                          length:      199,
                          play_count:  1214722172)
   end
@@ -14,13 +14,14 @@ RSpec.describe 'songs show' do
   it 'displays the song title' do
     visit "/songs/#{@song_1.id}"
 
-    expect(page).
+    expect(page).to have_content(@song_1.title)
+    expect(page).to_not have_content(@song_2.title)
   end
 
-  # it 'displays the song title' do
-  #
-  # end
-  #
+  it 'displays the artist name' do
+    
+  end
+
   # it 'takes me back to the songs index page' do
   #   #   As a user
   #   # When I visit a Song show page

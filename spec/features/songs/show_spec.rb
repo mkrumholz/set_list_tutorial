3e878b9003739e928368ffe19fcb94e44aadb63e
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-12 16:13:11 -0600

    Create test for songs#show and build basics

---
+++ spec/features/songs/show_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'songs show' do
+  before(:each) do
+    @song_1 = Song.create!(title:       "I Really Like You",
+                         length:      208,
+                         play_count:  243810867)
+    @song_2 = Song.create!(title:       "Call Me Maybe",
+                         length:      199,
+                         play_count:  1214722172)
+  end
+
+  it 'displays the song title' do
+    visit "/songs/#{@song_1.id}"
+
+
+  end
+
+  # it 'displays the song title' do
+  #
+  # end
+  #
+  # it 'takes me back to the songs index page' do
+  #   #   As a user
+  #   # When I visit a Song show page
+  #   # I see a link back to the songs index page
+  #   # When I click this link
+  #   # Then I am taken to the songs index
+  #
+  #
+  # end
+end
